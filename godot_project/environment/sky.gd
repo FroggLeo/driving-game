fc50e8368afff5bd332265b2b40465ed1a2da3f6
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-06 18:02:36 -0800

    add tilt to the sun and internal year counter

--- godot_project/environment/sky.gd
+++ godot_project/environment/sky.gd
@@ -3,7 +3,17 @@ extends WorldEnvironment
 # seconds in a day
 @export var day_length: float = 10
 # days to take the moon to do one cycle
-@export var moon_cycle: float = 8
+@export var moon_cycle: float = 29.5
+# days in a year
+@export var year_length: float = 10
+# tilt of the sun, axial tilt
+@export var axial_tilt: float = 23.5
+
+# nodes used
+@onready var sun = $sun_tilt/sun
+@onready var moon = $moon_tilt/moon
+@onready var sun_tilt = $sun_tilt
+@onready var moon_tilt = $moon_tilt
 
 # from 0 to 1
 # 0 is midnight, 0.5 is noon, 1 is midnight
@@ -11,28 +21,43 @@ extends WorldEnvironment
 var time: float = 0
 # the moon's rotation
 var moon_time: float = 0
+# the time of the year
+var year_time: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# calculate the correct time
-	time += delta / day_length
+	# global time change
+	var time_change = delta / day_length
+	
+	# calculate year time
+	year_time += time_change / year_length
+	if year_time >= 1.0:
+		year_time = 0
+	# sun tilt
+	# using sin so that it goes from -1 to 1 instead of 0 to 1
+	# trig...
+	var sun_tilt_angle = deg_to_rad(axial_tilt) * sin(year_time * TAU)
+	sun_tilt.rotation.z = sun_tilt_angle
+	
+	# calculate day time
+	time += time_change
 	if time >= 1.0:
 		time = 0
-	
 	# sun rotation
 	var sun_angle = TAU * time
-	$sun.rotation.x = sun_angle
+	sun.rotation.x = sun_angle
 	
-	# TODO moon cycles
-	# so the moon rises at different times of the day
-	# over an entire month, 
-	moon_time += delta / day_length / moon_cycle
+	# calculate moon time
+	moon_time += time_change / moon_cycle
+	if moon_time >= 1.0:
+		moon_time = 0
 	# the extra sun angle gives it somewhat realisic feel
 	# so that sometimes the moon can be seen in the day
-	$moon.rotation.x = moon_time * TAU + sun_angle
+	moon.rotation.x = moon_time * TAU + sun_angle
+	
+	
 	
