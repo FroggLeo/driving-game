52988a374e4bb6473cd75553b092516761d0ecda
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-06 19:09:23 -0800

    add tilt to the moon to cycle +- 5 degrees every 18.6 years

--- godot_project/environment/sky.gd
+++ godot_project/environment/sky.gd
@@ -3,11 +3,16 @@ extends WorldEnvironment
 # seconds in a day
 @export var day_length: float = 0.05
 # days to take the moon to do one cycle
-@export var moon_cycle: float = 29.5
+@export var moon_length: float = 29.5
 # days in a year
 @export var year_length: float = 10
-# tilt of the sun, axial tilt
+# tilt of the sun, axial tilt, difference between equinox and summer/winter solstice
 @export var axial_tilt: float = 23.5
+# tilt of the moon relative to the sun
+@export var moon_axial_tilt: float = 5.0
+# moon cycle time between +- 5 degrees, in years
+# just called it a moon season
+@export var moon_season_length: float = 18.6
 
 # nodes used
 @onready var sun = $sun_tilt/sun
@@ -23,13 +28,20 @@ var time: float = 0
 var moon_time: float = 0
 # the time of the year
 var year_time: float = 0
+# time of the moon cycle
+var moon_season: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# equinox of the tilt of the sun, degrees from the vertical
+	# 35 degrees is around los angeles's equinox
+	sun.rotation.x = deg_to_rad(35)
+	moon.rotation.x = deg_to_rad(35+180)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Global.paused:
+		return
 	# global time change
 	var time_change = delta / day_length
 	
@@ -52,12 +64,18 @@ func _process(delta: float) -> void:
 	sun.rotation.z = sun_angle
 	
 	# calculate moon time
-	moon_time += time_change / moon_cycle
+	moon_time += time_change / moon_length
 	if moon_time >= 1.0:
 		moon_time = 0
 	# the extra sun angle gives it somewhat realisic feel
 	# so that sometimes the moon can be seen in the day
-	moon.rotation.z = moon_time * TAU + sun_angle
+	var moon_angle = TAU * moon_time
+	moon.rotation.z = moon_angle + sun_angle
 	
-	
-	
+	# calculate moon season time
+	moon_season += (time_change / year_length) / moon_season_length
+	if moon_season >= 1.0:
+		moon_season = 0
+	# moon tilt
+	var moon_tilt_angle = sun_tilt_angle + (deg_to_rad(moon_axial_tilt) * sin(moon_season * TAU))
+	moon_tilt.rotation.x = moon_tilt_angle
