af27a9c383b618e8d9cbba7dcbe6bb86cce4d0c6
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-07 22:40:34 -0800

    use physical sky material instead of custom shader

--- godot_project/environment/sky.gd
+++ godot_project/environment/sky.gd
@@ -1,3 +1,4 @@
+@tool
 extends WorldEnvironment
 
 @export_category("Time lengths")
@@ -13,12 +14,21 @@ extends WorldEnvironment
 
 @export_category("Tilt of the sun and moon")
 # tilt of the sun, axial tilt, difference between equinox and summer/winter solstice
-@export var axial_tilt: float = 23.5
+@export var axial_tilt: float = 23.5:
+	set (value):
+		axial_tilt = value
+		_update_sun_moon()
 # tilt of the moon relative to the sun
-@export var moon_axial_tilt: float = 5.0
+@export var moon_axial_tilt: float = 5.0:
+	set (value):
+		moon_axial_tilt = value
+		_update_sun_moon()
 # tilt of the equinox from the vertical, or the degree of the latitude
 # 34 degrees is around los angeles's equinox
-@export var latitude: float = 34
+@export_range(-90,90,0.01) var latitude: float = 34:
+	set (value):
+		latitude = value
+		_update_sun_moon()
 
 @export_category("Sun and moon energy")
 # energy of the sunlight
@@ -27,6 +37,30 @@ extends WorldEnvironment
 # max fraction of the reflected sunlight @ full moon, 0 to 1
 @export var moon_energy: float = 0.136
 
+@export_category("Initial time")
+# all from 0 to 1
+# this is the initial amounts when the game starts
+# the initial time
+@export_range(0,1,0.001) var time: float = 0:
+	set (value):
+		time = value
+		_update_sun_moon()
+# the moon's rotation
+@export_range(0,1,0.001) var moon_time: float = 0:
+	set (value):
+		moon_time = value
+		_update_sun_moon()
+# the time of the year
+@export_range(0,1,0.001) var year_time: float = 0:
+	set (value):
+		year_time = value
+		_update_sun_moon()
+# time of the moon season
+@export_range(0,1,0.001) var moon_season: float = 0:
+	set (value):
+		moon_season = value
+		_update_sun_moon()
+
 # nodes used
 @onready var sun = $sun_tilt/sun
 @onready var moon = $moon_tilt/moon
@@ -34,18 +68,6 @@ extends WorldEnvironment
 @onready var moon_tilt = $moon_tilt
 @onready var sun_light = $sun_tilt/sun/sun_light
 @onready var moon_light = $moon_tilt/moon/moon_light
-
-@export_category("Initial time")
-# all from 0 to 1
-# this is the initial amounts when the game starts
-# the initial time
-@export_range(0,1,0.001) var time: float = 0
-# the moon's rotation
-@export_range(0,1,0.001) var moon_time: float = 0
-# the time of the year
-@export_range(0,1,0.001) var year_time: float = 0
-# time of the moon season
-@export_range(0,1,0.001) var moon_season: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -57,12 +79,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
 	if Global.paused:
 		return
 	
 	# calculate global time change
 	var time_change = delta / day_length
+	_update_clock(time_change)
 	
+	_update_sun_moon()
+
+func _update_clock(time_change) -> void:
 	# calculate day/sun time
 	time += time_change
 	if time >= 1.0:
@@ -79,10 +107,19 @@ func _process(delta: float) -> void:
 	moon_season += (time_change / year_length) / moon_season_length
 	if moon_season >= 1.0:
 		moon_season = 0
+
+func _update_sun_moon() -> void:
+	if sun == null or moon == null or sun_tilt == null or moon_tilt == null or sun_light == null or moon_light == null:
+		return
 	
 	# sun rotation
 	var sun_angle = TAU * time
 	sun.rotation.z = sun_angle
+	# sun tilt
+	# using sin so that it goes from 1 to -1 to 1 instead of 0 to 1
+	# trig...
+	var sun_tilt_angle = deg_to_rad(axial_tilt) * -sin((year_time - 0.25) * TAU)
+	sun_tilt.rotation.x = sun_tilt_angle
 	
 	# moon rotation
 	# the extra sun angle gives it somewhat realisic feel
@@ -92,13 +129,6 @@ func _process(delta: float) -> void:
 	# moon light energy
 	# make sure to place the sun and moon meshes opposite of each other
 	moon_light.light_energy = sun_energy * moon_energy * absf(2 * (0.5 - moon_time))
-	
-	# sun tilt
-	# using sin so that it goes from 1 to -1 to 1 instead of 0 to 1
-	# trig...
-	var sun_tilt_angle = deg_to_rad(axial_tilt) * sin(year_time * TAU)
-	sun_tilt.rotation.x = sun_tilt_angle
-	
 	# moon tilt
 	# based on sun tilt but varies by +-moon_axial_tilt over a time of moon_season years
 	var moon_tilt_angle = sun_tilt_angle + (deg_to_rad(moon_axial_tilt) * sin(moon_season * TAU))
