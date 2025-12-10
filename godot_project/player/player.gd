a3a3ea2d737dc949eea8a7726c0b95c34777a711
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-10 03:00:54 -0800

    refactor code and implement player rotation in 3rd person when walking

--- godot_project/player/player.gd
+++ godot_project/player/player.gd
@@ -2,16 +2,22 @@ extends CharacterBody3D
 
 var first_person: bool = false
 
+# nodes used
+@onready var first_person_cam = $first_person_cam
+@onready var third_person_cam = $third_person_spring/third_person_cam
+@onready var third_person_spring = $third_person_spring
+@onready var player_mesh = $mesh
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%third_person_spring.spring_length = 0
+	third_person_spring.spring_length = 0
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	%first_person_cam.make_current()
+	first_person_cam.make_current()
 	Global.paused = false
 
 func _unhandled_input(event):
 	
-	first_person = %third_person_spring.spring_length <= 0
+	first_person = third_person_spring.spring_length <= 0
 	
 	# release mouse on cancel event
 	if event.is_action_pressed("ui_cancel"):
@@ -32,28 +38,26 @@ func _unhandled_input(event):
 	# first person camera movement
 	if event is InputEventMouseMotion and first_person:
 		rotation_degrees.y -= event.relative.x * Global.sensitivity
-		%first_person_cam.rotation_degrees.x -= event.relative.y * Global.sensitivity
+		first_person_cam.rotation_degrees.x -= event.relative.y * Global.sensitivity
 		# limits for vertical rotation
-		%first_person_cam.rotation_degrees.x = clamp(%first_person_cam.rotation_degrees.x, -80, 80)
+		first_person_cam.rotation_degrees.x = clamp(first_person_cam.rotation_degrees.x, -80, 80)
 	
 	# auto switch camera
-	# TODO match camera rotation when switching cams
 	if first_person:
 		if event.is_action_pressed("zoom_out"):
-			%third_person_spring.spring_length = Global.zoom_inc
-			%third_person_spring.rotation = %first_person_cam.rotation
-			$third_person_spring/third_person_cam.make_current()
+			third_person_spring.spring_length = Global.zoom_inc
+			third_person_spring.rotation = first_person_cam.rotation
+			third_person_cam.make_current()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		elif get_viewport().get_camera_3d() == $third_person_spring/third_person_cam:
-			rotation.y = %third_person_spring.global_transform.basis.get_euler().y
-			%first_person_cam.rotation.x = %third_person_spring.rotation.x
-			%first_person_cam.rotation.z = %third_person_spring.rotation.z
-			%first_person_cam.make_current()
+		elif get_viewport().get_camera_3d() == third_person_cam:
+			rotation.y = third_person_spring.global_transform.basis.get_euler().y
+			first_person_cam.rotation.x = third_person_spring.rotation.x
+			first_person_cam.rotation.z = third_person_spring.rotation.z
+			first_person_cam.make_current()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# Don't use for tick events, will break game if framerate is not expected
 #func _process(delta: float) -> void:
 #	pass
 
@@ -73,11 +77,12 @@ func _physics_process(delta):
 	
 	if first_person:
 		var input_direction_3D = Vector3(input_direction_2D.x, 0.0, input_direction_2D.y)
+		# gets the player rotation
 		direction = transform.basis * input_direction_3D
 		direction.y = 0
 	else:
 		# gets the spring arm rotation
-		var cam_rotation = %third_person_spring.global_transform.basis
+		var cam_rotation = third_person_spring.global_transform.basis
 		
 		# only need to do forward and right
 		var forward = cam_rotation.z
@@ -86,17 +91,20 @@ func _physics_process(delta):
 		right.y = 0
 		
 		direction = (right * input_direction_2D.x) + (forward * input_direction_2D.y)
+		
+		# 3rd person rotation code
+		# TODO need to make it workkkkkkk
+		if direction.length() > 0.01:
+			# why is it off by 45 degrees??
+			var walking_direction = atan2(direction.x,direction.z)
+			player_mesh.rotation.y = walking_direction
+			first_person_cam.rotation.y = walking_direction
 	
 	# this line like prevents the player from moving faster when they are going diagonally
 	direction = direction.normalized()
 	# apply the calculated speeds based on direction
 	velocity.x = direction.x * WALK_SPEED
 	velocity.z = direction.z * WALK_SPEED
-	
-	# TODO fix this bro
-	#if not first_person:
-	#	var player_rotation = atan2(-direction.x, -direction.z)
-	#	$Node3D.rotation.y += player_rotation
 	
 	# jumping code
 	if not is_on_floor():
