88d55c6f3c8d392769ba9944702d041737024384
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-12 00:20:59 -0800

    add some comments for car.gd

--- godot_project/car/car.gd
+++ godot_project/car/car.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var max_speed: float = 20.0
-@export var acceleration_decel: float = 12.0
+@export var acceleration: float = 12.0
 @export var braking_decel: float = 8.0
 @export var rolling_decel: float = 3.0
 
@@ -26,8 +26,10 @@ var brake: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# make it a group to be interacted with in the world
 	add_to_group("interactable")
 	third_person_spring.spring_length = third_person_zoom
+	# don't set current camera to the car
 	driver_cam.current = false
 	third_person_cam.current = false
 
@@ -45,6 +47,21 @@ func _unhandled_input(event):
 func _physics_process(delta):
 	if Global.paused:
 		return
+	# if there is no driver
+	if driver == null:
+		return
+	
+	if Input.is_action_just_pressed("interact"):
+		exit()
+		return
 	
 	
 	move_and_slide()
+
+func enter() -> void:
+	# TODO set driver to a player?
+	pass
+
+func exit() -> void:
+	# set driver to nothing
+	driver = null
