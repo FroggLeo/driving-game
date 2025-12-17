7cc8cac4108def4e04acaabda7a91fac25b5bb1b
Leo <88123479+FroggLeo@users.noreply.github.com>
2025-12-17 03:22:17 -0800

    add all the other forces, add them together for forward and reverse!

--- godot_project/car/car.gd
+++ godot_project/car/car.gd
@@ -14,13 +14,17 @@ extends RigidBody3D
 @export var min_speed: float = 1.0
 # maximum force when braking
 @export var max_brake_force: float = 9000.0
-# natural engine brake force when the petal is lifted
-@export var engine_brake_force: float = 300.0
 
-# force of the roll is the mass of the car * coef * gravity
-@export var rolling_resistance_coef: float = 200.0
+# natural engine brake force when the petal is lifted
+# simple modeling using this function:
+# force = -brake_force_max * v / (v + brake_fade_speed)
+@export var engine_brake_force_max: float = 1000.0
+@export var engine_brake_fade_speed: float = 25.0
+# force of the roll is the mass * coef * gravity
+@export var rolling_resistance_coef: float = 0.012
 # aerodynamic drag constant
-@export var drag_coef: float = 0.6
+@export var drag_coef: float = 0.55
+@export var air_density: float = 1.225
 
 # mass of the truck in kg
 @export var car_mass: float = 1700.0
@@ -31,12 +35,6 @@ extends RigidBody3D
 @export var long_cof: float = 0.85
 # gravity
 @export var gravity: float = 9.81
-
-# change in steering per second, steering goes from -1 to 1
-# steering speed when the car is stopped
-@export var steering_speed_00: float = 1.0
-# steering speed at 30m/s
-@export var steering_speed_30: float = 0.4
 
 # dedzone of the input
 @export var deadzone: float = 0.01
@@ -52,6 +50,7 @@ extends RigidBody3D
 #@onready var car_mesh = $mesh
 
 var riders: Array[CharacterBody3D] = []
+var total_mass := car_mass # add item masses here too
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -77,9 +76,16 @@ func _physics_process(delta):
 	var s := v.length()
 	var forward := -global_transform.basis.z
 	var right := global_transform.basis.x
+	# speed along the forward direction, -1 to 1
+	var v_forward := v.dot(forward)
 	
 	# the overall drive input
 	var drive_input := throttle_input - reverse_input
+	
+	# steer
+	if abs(steer_input) > deadzone and abs(v_forward) > 0.1:
+		pass
+		
 	
 	# forward and reverse throttle
 	# reverse will simply apply a negative force to 'brake'
@@ -89,20 +95,32 @@ func _physics_process(delta):
 		# using formula engine_force = (power * input) / velocity
 		var total_power = max_power_watts * drivechain_efficiency
 		drive_force = (total_power * drive_input) / max(min_speed, s)
-		print("calculated drive input: " + str(drive_input))
-		print("calculated drive force: " + str(drive_force))
+		#print("calculated drive input: " + str(drive_input))
+		#print("calculated drive force: " + str(drive_force))
 	
-	# steer
-	if abs(steer_input) > deadzone:
-		pass
-	
-	# brake
+	# brakes
+	var brake_force: float = 0.0
 	if brake_input > deadzone:
-		var 
-		
+		brake_force = max_brake_force * brake_input * -sign(v_forward)
 	
-	# apply all forces
-	apply_central_force(forward * drive_force)
+	# coasting force, no pedals down
+	var engine_force: float = 0.0
+	if abs(drive_input) <= deadzone and abs(v_forward) > 0.01:
+		engine_force = -engine_brake_force_max * s / (s + engine_brake_fade_speed)
+	
+	# rolling resistance and aerodynamic drag
+	var resist_force: float = 0.0
+	if s > 0.01:
+		# drag calculation based on the formula
+		var drag_force = air_density * s * s * 4 * drag_coef / 2
+		var roll_force = rolling_resistance_coef * total_mass * gravity
+		# -sign(v_forward) allows us to apply the force in the opposite direction of the movement
+		resist_force = (drag_force + roll_force) * -sign(v_forward)
+	
+	# add all da forces
+	var total_long_force = drive_force + brake_force + engine_force + resist_force
+	# apply the force
+	apply_central_force(forward * total_long_force)
 
 func _body_entered(body: Node) -> void:
 	if body is CharacterBody3D and body.has_method("set_interactable"):
